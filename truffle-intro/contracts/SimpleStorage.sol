4b8b9afe483ce1e9e9a0c46597313426f3807d02
achie27 <architmathur2011@gmail.com>
2022-04-18 23:40:10 +0530

    feat: adds external network in config and an event in SimpleStorage

--- truffle-intro/contracts/SimpleStorage.sol
+++ truffle-intro/contracts/SimpleStorage.sol
@@ -4,9 +4,12 @@ pragma solidity >=0.4.22 <0.9.0;
 contract SimpleStorage {
   uint storedData;
 
+  event setEvent(uint newVal);
+
   function set(uint x) public {
     uint tmp = 3 * x + 2;
     storedData = tmp;
+    emit setEvent(storedData);
   }
 
   function get() public view returns (uint) {
